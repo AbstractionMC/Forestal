b0be5fb6b9d56799298d9a78d42ef4ed6a8364e2
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-04-13 16:23:49 +0200

    Fix setup

---
+++ src/client/java/net/rotgruengelb/forestal/ForestalClient.java
@@ -0,0 +1,10 @@
+package net.rotgruengelb.forestal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForestalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/rotgruengelb/forestal/ForestalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rotgruengelb.forestal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ForestalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
